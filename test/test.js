036f9e78fa9a3a8afc08d53f4383118593f84cd3
strk2333 <strk23@outlook.com>
2019-04-03 13:58:33 +0800

    test fold

---
+++ test/test.js
@@ -0,0 +1,281 @@
+// import XMLParser from './xml-parser/xmlParser'
+
+const XMLParser = require('../xmlParser')
+
+const res = `<div><p>11</p><p>2<br>2</p><p>33</p></div>`
+const testRes = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Balanced
+Description: A two-column, fixed-width design suitable for blogs and small websites.
+Version    
+Released   : 20090927
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>STUDIO48 ♪ [ Kaerimichi wa toomawari shitaku naru ]</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="style_lyric.css" rel="stylesheet" type="text/css" media="screen" />
+<style type="text/css">
+<!--
+.style1 {color: #cc0099}
+.style2 {color: #0099FF}
+.style3 {color: #00cc66}
+.style4 {color: #ff3399}
+.style5 {color: #cc9900}
+.style6 {color: #CC99FF}
+.style7 {color: #DD33DD}
+.style8 {color: #003399}
+.style9 {color: #0000ff}
+.style10 {color: #993300}
+.style11 {color: #ff9900}
+.style12 {color: #ff00ff}
+.style13 {color: #6666cc}
+.style14 {color: #99cc00}
+.style15 {color: #ff99cc}
+.style16 {color: #ffcc00}
+.style17 {color: #9900ff}
+.style18 {color: #006600}
+.style19 {color: #FF0000}
+.style20 {color: #00FFFF}
+.style21 {color: #009999}
+.style22 {color: #FFCC66}
+.style23 {color: #CC9999}
+.style24 {color: #FF6600}
+.style25 {color: #FF9999}
+.style26 {color: #FF0066}
+.style27 {color: #00FF99}
+.style28 {color: #99CCCC}
+.style29 {color: #330066}
+.style30 {color: #3366FF}
+.style31 {color: #00FF00}
+.style32 {color: #FFCCFF}
+.style33 {color: #FFCC99}
+.style34 {color: #666699}
+.style35 {color: #FF66CC}
+.style36 {color: #33CCFF}
+.style37 {color: #800080}
+.style38 {color: #CC00FF}
+.style39 {color: #CCECFF}
+.style40 {color: #CC66CC}
+.style41 {color: #33CC66}
+.style42 {color: #FF6699}
+.style43 {color: #339900}
+.style44 {color: #fff600}
+.style45 {color: #d3cc03}
+.style46 {color: #05c6ad}
+-->
+</style>
+</head>
+<body>
+<!-- start header -->
+<div class="header">
+	<div id="menu">
+		<ul>
+			<a href="index.html">HOME</a>
+            <a href="discography.html">DISCOGRAPHY</a>
+            <a href="lyricsindex.html">LYRICS INDEX</a>
+            <a href="http://www.stage48.net/">STAGE48</a>
+            <a href="contactme.html">CONTACT ME</a>
+		</ul>
+	</div>
+</div>
+<!-- end header -->
+<!-- start page -->
+<div id="page">
+	<!-- start content -->
+<div id="content">
+		<div class="post greenbox">
+			<div class="title">
+				<h1>帰り道は遠回りしたくなる | Kaerimichi wa toomawari shitaku naru</h1>
+		  </div>
+			<div class="entry">
+			  <table width="916" border="0">
+                <tr>
+                  <td width="916" align="center" valign="top">
+                  <strong>Romaji:</strong> haruna_hamasaki | <strong>Color-code:</strong> - | <strong>English Translation:</strong> -<br /><br />
+                  <strong>Information:</strong><br />
+                  Nogizaka46 22nd Single - Kaerimichi wa toomawari shitaku naru<br /><br />
+				  <strong>Selected Members:</strong><br />
+                  -</td>
+                </tr>
+              </table>
+			  </div>
+	  <div class="btm">
+				<div class="l">
+					<div class="r">
+					  <p class="meta"></p>
+					</div>
+				</div>
+		  </div>
+		</div>
+		<div class="two-columns">
+			<div class="column">
+				<div class="title pink">
+					<h2>kanji & kana</h2>
+				</div>
+				<div class="content content2 content3">
+					<ul>
+						<p>好きだった…　この場所…</p>
+						<p>やめられない漫画を途中で閉じて<br />
+						  顔を上げて気づくように<br />
+						  居心地いい日向もいつの間にか<br />
+						  影になって黄昏る</p>
+						<p>君と会って<br />
+						  過ぎる時間忘れるくらい夢中で話した<br />
+						  僕の夢は<br />
+						  ここではないどこかへ</p>
+						<p>帰り道は　帰り道は<br />
+						  遠回りをしたくなるよ<br />
+						  どこを行けば　どこに着くか？<br />
+						  過去の道なら迷うことがないから<br />
+						  弱虫(弱虫…)<br />
+						  新しい世界へ<br />
+						  今　行きたい　行きたい　行きたい　行きたい<br />
+						  強くなりたい</p>
+						<p>Oh！Oh！Oh！ 好きだった…　この場所…<br />
+						  Oh！Oh！Oh！ 一歩目…　踏み出そう！</p>
+						<p>街灯りが寂しくふと感じるのは<br />
+						  見慣れた景色と違うから<br />
+						  いつもの高架線が見えなくなって<br />
+						  どこにいるかわからない</p>
+						<p>人は誰も<br />
+						  変わることに慣れていなくて昨日と同じように<br />
+						  今日も明日も ここにいたくなるんだ</p>
+						<p>知らない道 知らない道<br />
+						  あと何回 歩けるだろう<br />
+						  夢の方へ 愛の方へ<br />
+						  風は道を選んだりはしないよ<br />
+						  このまま(このまま…)<br />
+						  ONE WAYの標識<br />
+						  でも行くんだ 行くんだ 行くんだ 行くんだ<br />
+						  戻れなくても…</p>
+						<p>君と離れるのは悲しいけど<br />
+						  大事な別れだ<br />
+						  もっともっと広い世界<br />
+						  知らなきゃいけない<br />
+						  いつか(いつか)きっと(きっと)<br />
+						  違う道を選んだ意味<br />
+						  輝く未来のためと 互いにわかるだろう</p>
+						<p>風のように　風のように<br />
+						  思うままに生きてみよう<br />
+						  過去がどんな眩しくても<br />
+						  未来はもっと眩しいかもしれない</p>
+						<p>帰り道は　帰り道は<br />
+						  遠回りをしたくなるよ<br />
+						  どこを行けば　どこに着くか？<br />
+						  過去の道なら迷うことがないから<br />
+						  弱虫(弱虫…)<br />
+						  新しい世界へ<br />
+						  今 行きたい 行きたい 行きたい 行きたい<br />
+						  強くなりたい</p>
+						<p>Oh！Oh！Oh！ 好きだった… この場所…<br />
+						  Oh！Oh！Oh！ 大切な… 思い出…<br />
+						  Oh！Oh！Oh！ 好きだった… この場所…</p>
+					</ul>
+				</div>
+          </div>
+          <div class="column">
+				<div class="title pink">
+					<h2>romaji</h2>
+			</div>
+				<div class="content content2 content3">
+					<ul>
+						<p>suki datta… kono basho…</p>
+						<p>yamerarenai manga wo tochuu de tojite<br />
+						  kao wo agete kidzuku you ni<br />
+						  igokochi ii hinata mo itsu no ma ni ka<br />
+						  kage ni natte tasogareru</p>
+						<p>kimi to atte<br />
+						  sugiru jikan wasureru kurai muchuu de hanashita<br />
+						  boku no yume wa<br />
+						  koko de wa nai doko ka e</p>
+						<p>kaerimichi wa kaerimichi wa <br />
+						  toomawari wo shitaku naru yo<br />
+						  doko wo yukeba doko ni tsuku ka?<br />
+						  kako no michi nara mayou koto ga nai kara<br />
+						  yowamushi (yowamushi…)<br />
+						  atarashii sekai e <br />
+						  ima yukitai yukitai yukitai yukitai <br />
+						  tsuyoku naritai</p>
+						<p>Oh! Oh! Oh! suki datta… kono basho…<br />
+						  Oh! Oh! Oh! ippome… fumidasou!</p>
+						<p>machiakari ga samishiku futo kanjiru no wa<br />
+						  minareta keshiki to chigau kara<br />
+						  itsumo no koukasen ga mienaku natte<br />
+						  doko ni iru ka wakaranai</p>
+						<p>hito wa dare mo<br />
+						  kawaru koto ni narete inakute kinou to onaji you ni <br />
+						  kyou mo asu mo<br />
+						  koko ni itaku narunda</p>
+						<p>shiranai michi shiranai michi<br />
+						  ato nankai arukeru darou<br />
+						  yume no hou e ai no hou e<br />
+						  kaze wa michi wo erandari wa shinai yo<br />
+						  kono mama (kono mama…)<br />
+						  ONE WAY no hyoushiki<br />
+						  demo yukunda yukunda yukunda yukunda<br />
+						  modorenakute mo…</p>
+						<p>kimi to hanareru no wa kanashii kedo<br />
+						  daiji na wakare da<br />
+						  motto motto hiroi sekai<br />
+						  shiranakya ikenai<br />
+						  itsuka (itsuka) kitto (kitto)<br />
+						  chigau michi wo eranda imi<br />
+						  kagayaku mirai no tame to<br />
+						  tagai ni wakaru darou </p>
+						<p>kaze no you ni kaze no you ni<br />
+						  omou mama ni ikite miyou<br />
+						  kako ga donna mabushikute mo<br />
+						  mirai wa motto mabushii ka mo shirenai</p>
+						<p>kaerimichi wa kaerimichi wa<br />
+						  toomawari wo shitaku naru yo <br />
+						  doko wo yukeba doko ni tsuku ka? <br />
+						  kako no michi nara mayou koto ga nai kara <br />
+						  yowamushi (yowamushi…)<br />
+						  atarashii sekai e <br />
+						  ima yukitai yukitai yukitai yukitai<br />
+						  tsuyoku naritai </p>
+						<p>Oh! Oh! Oh! suki datta… kono basho…<br />
+						  Oh! Oh! Oh! taisetsu na… omoide…<br />
+						  Oh! Oh! Oh! suki datta… kono basho…</p>
+					</ul>
+				</div>
+                        </div>
+                        <div class="column">
+				<div class="title pink">
+					<h2>english translation</h2>
+				</div>
+				<div class="content content2 content3">
+					<ul>
+						<p>-</p>
+					</ul>
+			</div>
+		  </div>
+			<div class="btm">&nbsp;</div>
+		</div>
+	</div>
+	<!-- end content -->
+</div>
+<!-- end page -->
+<div style="clear: both;">&nbsp;</div>
+<div id="footer">
+	<p>&copy;2007 AKB48 Lyrics Paradise. &copy;2009 STUDIO48. All Rights Reserved. Original Design by <a href="http://www.freecsstemplates.org/">FCT</a> &amp; Edited by <a href="ayuchan.momopi@gmail.com">haruna_hamasaki</a>.</p>
+</div>
+</body>
+</html>
+`
+const parser = new XMLParser()
+const xml = parser.parseFromString(testRes, '(<br \/>|<br\/>|<br>|<br >)')
+const arr = xml.getElementsByTagName('div')
+const arr2 = arr.filter((it) => {
+    return it.attributes && it.attributes['class'] && it.attributes['class'].indexOf('content2') != -1
+})
+
+console.log(parser.toString(arr2[0]))
